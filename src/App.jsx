f239e017e6bf8facdd7eca27340b7e8b580b7af1
ksa607 <karine.samyn@hogent.be>
2025-09-28 19:33:40 +0200

    Refactor TransactionList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Transaction from './components/transactions/Transaction';
 import PlacesList from './components/places/PlacesList';
-import { TRANSACTION_DATA } from './api/mock_data';
+import TransactionList from './components/transactions/TransactionsList';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-center mb-4">
-        Budget App
-      </h1>
-      {TRANSACTION_DATA.map((t) => (<Transaction {...t} key={t.id} />))}
+    <div className="bg-white text-gray-900">
+      <TransactionList />
       <PlacesList />
     </div>);
 }
